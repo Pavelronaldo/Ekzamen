7bf02959c29fbc0a4514519689a1759622bca85f
Pavel <pavel.m.3107@gmail.com>
2020-06-10 10:15:45 +0300

    Initial Commit

---
+++ Ekzamen/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Ekzamen
+//
+//  Created by Pavel Ronaldo on 6/10/20.
+//  Copyright © 2020 Pavel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
